a874c296a3bfafeb0aa0408f7f56e119bd250e42
senin8 <malizziaaries@gmail.com>
2018-05-21 23:35:35 +0200

    Add project files.

---
+++ Ejercicio3/Menu.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio3
+{
+    public class Menu
+    {
+        public int HamburguesaSencilla;
+        public int HamburguesaQueso;
+        public int HamburguesaEspecial;
+        public int PatatasFritas;
+        public int Refresco;
+        public int Postre;
+
+        public void AddHamburguesaSencilla()
+        {
+            HamburguesaSencilla++;
+        }
+        public void AddHamburguesaQueso()
+        {
+            HamburguesaQueso++;
+        }
+        public void AddHamburguesaEspecial()
+        {
+            HamburguesaEspecial++;
+        }
+        public void AddPatatasFritas()
+        {
+            PatatasFritas++;
+        }
+        public void AddRefresco()
+        {
+            Refresco++;
+        }
+        public void AddPostre()
+        {
+            Postre++;
+        }
+
+        public int CalcularTotal()
+        {
+            int total = 0;
+
+            total += HamburguesaSencilla * 5;
+            total += HamburguesaQueso * 8;
+            total += HamburguesaEspecial * 10;
+            total += PatatasFritas * 3;
+            total += Refresco * 2;
+            total += Postre * 3;
+
+            return total;
+        }
+
+    }
+}
---
+++ Ejercicio3/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instancia menú
+            
+            Menu menu = new Menu();
+            //se crea un string continuar para indicar si continúa haciendo el while
+            string continuar = "si";
+            Console.WriteLine("Menu: ");
+
+
+            while (continuar == "si")
+            {
+                Console.WriteLine("1. Hamburguesa Sencilla (5€)");
+                Console.WriteLine("2. Hamburguesa con Queso (8€)");
+                Console.WriteLine("3. Hamburguesa Especial (10€)");
+                Console.WriteLine("4. Patatas Fritas (3€)");
+                Console.WriteLine("5. Refresco (2€)");
+                Console.WriteLine("6. Postre (3€)");
+                Console.WriteLine("7. Salir");
+                Console.Write("¿Qué desea añadir?(elija el número del producto)");
+
+                string input = Console.ReadLine();
+                int eleccion = Convert.ToInt32(input);
+                switch (eleccion)
+                {
+                    case 1:
+                        menu.AddHamburguesaSencilla();
+                        break;
+                    case 2:
+                        menu.AddHamburguesaQueso();
+                        break;
+                    case 3:
+                        menu.AddHamburguesaEspecial();
+                        break;
+                    case 4:
+                        menu.AddPatatasFritas();
+                        break;
+                    case 5:
+                        menu.AddRefresco();
+                        break;
+                    case 6:
+                        menu.AddPostre();
+                        break;
+                    case 7:
+                        continuar = "no";
+                        break;
+                    default:
+                        break;
+                }
+
+
+            }
+
+            Console.WriteLine("El total de su pedido es de: {0}", menu.CalcularTotal());
+
+
+            Console.Read();
+        }
+    }
+}
+//EJERCICIO 3
+//El siguiente es el menú de un restaurante de hamburguesas.Elabora un sistema capaz de leer la cantidad de cada alimento y calcular la cuenta total de cada pedido de 
+//acuerdo al precio de cada uno de los productos.
+//Hamburguesa sencilla (5€)
+//Hamburguesa con queso(8€)
+//Hamburguesa especial(10€)
+//Patatas fritas(3€)
+//Refresco(2€)
+//Postre(3€)
